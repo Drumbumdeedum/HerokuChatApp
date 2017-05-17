10fbf16fdd92c435e1ed1a666cc9520735053d38
Daniel Szalay <sza.dan.uj@gmail.com>
2017-05-17 11:36:43 +0200

    add gradle files

---
+++ src/main/java/com/greenfox/chatapp/controller/ChatRestController.java
@@ -0,0 +1,13 @@
+package com.greenfox.chatapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatRestController {
+
+  @GetMapping("/")
+  public String index() {
+    return "FUCKING WORK ALREADY!!!";
+  }
+}
