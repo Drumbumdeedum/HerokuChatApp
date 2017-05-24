9c9bff480da73e49d2c70e5acb773448b6dda650
Daniel Szalay <sza.dan.uj@gmail.com>
2017-05-24 17:42:49 +0200

    update1874512

--- src/main/java/com/greenfox/chatapp/ChatAppSettings.java
+++ src/main/java/com/greenfox/chatapp/ChatAppSettings.java
@@ -3,7 +3,7 @@ package com.greenfox.chatapp;
 public class ChatAppSettings {
   private static final String CHAT_APP_LOGLEVEL = "INFO";
   private static final String CHAT_APP_UNIQUE_ID = "https://fackingawesomechatapp.herokuapp.com/api/message/receive";
-  private static final String CHAT_APP_PEER_ADDRESS = "https://limitless-journey-84827.herokuapp.com/api/message/receive";
+  private static final String CHAT_APP_PEER_ADDRESS = "https://greenfox-chat-app.herokuapp.com/api/message/receive";
 
   /*"https://greenfox-chat-app.herokuapp.com/api/message/receive"*/
   /*"https://limitless-journey-84827.herokuapp.com/api/message/receive"*/
